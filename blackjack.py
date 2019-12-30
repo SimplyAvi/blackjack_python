74e7ea4ec44335c9221d87cd6d897047809f445a
SimplyAvi <a.totaram@gmail.com>
2019-12-30 12:16:19 -0500

    fixed an error after editing while commenting some code out

--- blackjack.py
+++ blackjack.py
@@ -66,16 +66,16 @@ class Hand:
             
 
 
-test_deck = Deck()
-test_deck.shuffle()
-test_player = Hand()
-test_player.add_card(test_deck.deal())
-test_player.add_card(test_deck.deal())
-test_player.value
-print(test_player.value)
+# test_deck = Deck()
+# test_deck.shuffle()
+# test_player = Hand()
+# test_player.add_card(test_deck.deal())
+# test_player.add_card(test_deck.deal())
+# test_player.value
+# print(test_player.value)
 
-for card in test_player.cards:
-    print(card)
+# for card in test_player.cards:
+#     print(card)
 
 
 class Chips:
